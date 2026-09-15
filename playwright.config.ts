dfa9672ea4dc1be7cabcecb43baa74a6e32b5980
reshma-658 <reshmaelizabeth14@gmail.com>
2026-09-15 12:49:55 +0200

    Enable headless mode only in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    headless: !!process.env.CI,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
